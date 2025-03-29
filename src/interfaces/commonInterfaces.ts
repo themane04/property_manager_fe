9e406e9f8160b90b948a21eb5a971d281835de60
Marjan Tomev <tomevmarjan33@gmail.com>
2025-03-29 17:11:27 +0100

    - removed unused ItemList.tsx

--- src/interfaces/commonInterfaces.ts
+++ src/interfaces/commonInterfaces.ts
@@ -18,15 +18,3 @@ export interface DynamicFormProps<T extends Record<string, any>> {
     onSubmit: () => void;
     editId: string | null;
 }
-
-export interface BaseItem {
-    id: string | number;
-    name: string;
-}
-
-export interface ItemListProps<T extends BaseItem> {
-    title: string;
-    data: T[];
-    onEdit: (item: T) => void;
-    onDelete: (id: T['id']) => void;
-}
